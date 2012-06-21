e284c7eb8c7c03ad7f965433b4bfd3e65a0af558
Lucia <curso_ruby@fenix.(none)>
2012-06-20 23:40:59 -0300

    Se continua el desarrollo del cliente para twitter y se hace un esqueleto para tests unitarios.

--- twitter_rest_client.rb
+++ twitter_rest_client.rb
@@ -1,38 +1,68 @@
 require 'net/http'
-#http://ruby-doc.org/stdlib-1.9.3/libdoc/net/http/rdoc/Net/HTTP.html
+require 'json'
 
 class TwitterRestClient
 
 	def get_trends
-		get('https://api.twitter.com/1/trends/daily.json')
-		#Cambiar por https
-		#http://api.twitter.com/1/trends/daily.json
+		return get('http://api.twitter.com/1/trends/daily.json')
 	end
 	
 	def get_twits_for_trend(trend)
-		trend.strip
-		#http://search.twitter.com/search.json?q=#honestyhour
+		return get('http://search.twitter.com/search.json', {'q' => trend.strip})
 	end
 	
 	def get_twit_by_id(id)
-		#https://api.twitter.com/1/statuses/show/130022282960715776.json
+		return get("http://api.twitter.com/1/statuses/show/#{id.to_s.strip}.json")
 	end
 	
 	def get_user_information_by_id(id)
-		#Doc> https://dev.twitter.com/docs/api/1/get/users/lookup
-		#Doc> https://dev.twitter.com/docs/api/1/get/users/profile_image/%3Ascreen_name
-		# https://api.twitter.com/1/users/lookup.json?user_id=444
-		# https://api.twitter.com/1/users/profile_image?screen_name=twitterapi&size=bigger 
+		return get('http://api.twitter.com/1/users/lookup.json', {'user_id' => id})
 	end
 	
+	##
+	# Tries to get information from a given url and parameters. 
+	# If the response is successful it returns an array/hash representation of the data
+	# otherwise a TwitterRestClientException is raised
+	##
 	def get(uri, params={})
 		uri = URI(uri)
 		uri.query = URI.encode_www_form(params)
+		p uri
 		res = Net::HTTP.get_response(uri)
-		puts res.body if res.is_a?(Net::HTTPSuccess)
-	end
+		if res.is_a?(Net::HTTPSuccess)
+			response = res.body
+			return JSON.parse(response)
+		else 
+			raise TwitterRestClientException.new(res.code), "#{res.code}: #{res.message}"
+		end
+	end	
 	
 end
 
-t = TwitterRestClient.new
-t.get_trends
+##
+# Represents a server response different to 200
+##
+class TwitterRestClientException < StandardError
+
+	attr :code
+	
+  def initialize(code)
+    @code = code
+  end
+  
+  def not_found?
+  	return @code == 404
+  end
+  
+  def server_error?
+  	return !@code.to_s[/\A5/].nil?
+  end
+  
+end
+
+##
+# Error thrown when the value of a parameter used in a uri is invalid
+##
+class TwitterRestClientArgumentException < StandardError
+
+end
